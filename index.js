41e32b78a4a585b28b4fdb4543335bea57229978
sadhana <sadhana903545@gmail.com>
2025-06-15 23:22:51 +0530

    second commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/protect", router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.....`);
